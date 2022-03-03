d022b8d3d173af0da7d08992ac1ed5b5b7d7cdab
mxdi ♡ <78393312+madi11305@users.noreply.github.com>
2022-03-03 09:47:21 -0800

    Create updbashrc.sh

---
+++ Scripts/updbashrc.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+# Syncs local .bashrc with GitHub
+wget https://raw.githubusercontent.com/madi11305/dotfiles/master/.bashrc
+mv ~/.bashrc ~/temp
+mv .bashrc.1 ~/.bashrc
+source ~/.bashrc
+rm ~/temp
+echo "Complete"
